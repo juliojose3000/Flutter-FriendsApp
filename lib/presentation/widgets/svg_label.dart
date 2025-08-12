919615ac479c0cbb344251052ee9543df7f7c71e
Julidev <juliodev9810@gmail.com>
2025-08-11 19:25:42 -0600

    feat -> Make PostActionButtons widgets stateful

--- lib/presentation/widgets/svg_label.dart
+++ lib/presentation/widgets/svg_label.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:vector_graphics/vector_graphics_compat.dart';
 
-class SvgLabel extends StatelessWidget {
-
+class SvgLabelButton extends StatelessWidget {
   final String assetName;
   final String text;
   final Color color;
+  final VoidCallback? onPressed;
 
-  const SvgLabel({super.key, required this.assetName, required this.text, required this.color});
+  const SvgLabelButton({
+    super.key,
+    required this.assetName,
+    required this.text,
+    required this.color,
+    this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        VectorGraphic(
-          loader: AssetBytesLoader('assets/$assetName'),
-          width: 24,
-          height: 24,
-          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        ),
-        const SizedBox(width: 8),
-        Text(text, style: const TextStyle(fontSize: 16, color: Colors.black)),
-
-      ],
+    return GestureDetector(
+      onTap: () => {onPressed?.call()},
+      child: Row(
+        children: [
+          VectorGraphic(
+            loader: AssetBytesLoader('assets/$assetName'),
+            width: 24,
+            height: 24,
+            colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+          ),
+          const SizedBox(width: 8),
+          Text(text, style: const TextStyle(fontSize: 16, color: Colors.black)),
+        ],
+      ),
     );
   }
 }
