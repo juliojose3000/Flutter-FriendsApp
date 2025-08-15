24323087728933fd2cd62e8c85c2d8ebfbd54260
Julio Segura <julidev9810@gmail.com>
2025-08-15 10:32:06 -0600

    feat -> Navigation to UserProfileScreen

---
+++ lib/presentation/screens/user_profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatelessWidget {
+  final String userId;
+
+  const UserProfileScreen({super.key, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
+      body: Center(
+        child: Text('Profile of user with ID: $userId'),
+      ),
+    );
+  }
+}
